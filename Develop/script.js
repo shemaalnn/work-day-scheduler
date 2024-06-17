8ddfcfedda9af20c04437a1afd19fa04fdfaaf5e
Shema <shema.aln@outlook.com>
2024-06-17 22:55:52 +0100

    updated html and starting on js

---
+++ Develop/script.js
@@ -0,0 +1,9 @@
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
+
+
+// loading html before function is executed 
+$(document).ready(function () {
+  function currentDay()
+})
